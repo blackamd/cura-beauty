e71e1f31fa05f908e308763330a5b9f0a68a0e00
ahmed-12-soliman <ahmed.12.soliman@gmail.com>
2022-02-22 18:08:51 +0200

    update: return tamplet

--- frontend/index.php
+++ frontend/index.php
@@ -1,13 +1,13 @@
 <?php
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
-print_r("Hi from index");
-exit();
+
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 require(__DIR__ . '/../common/config/bootstrap.php');
 require(__DIR__ . '/config/bootstrap.php');
-
+print_r("Hi from index2");
+exit();
 $config = yii\helpers\ArrayHelper::merge(
     require(__DIR__ . '/../common/config/main.php'),
     require(__DIR__ . '/config/main.php'),
