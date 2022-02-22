a32c99e253184ce301ddaf6591d019e8f6655a09
ahmed-12-soliman <ahmed.12.soliman@gmail.com>
2022-02-22 15:53:57 +0200

    update: display error

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -4,11 +4,6 @@
 /** @var string $content */
 use frontend\assets\AppAsset;
 use yii\helpers\Url;
-use common\widgets\Alert;
-use yii\bootstrap4\Breadcrumbs;
-use yii\bootstrap4\Html;
-use yii\bootstrap4\Nav;
-use yii\bootstrap4\NavBar;
 
 AppAsset::register($this);
 ?>
@@ -19,18 +14,8 @@ AppAsset::register($this);
        <meta charset="<?= Yii::$app->charset ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" href="<?= Url::to(['../frontend/web/images/favicon.ico'])?>" type="image/x-icon"/>
-    <link rel="icon" href="<?= Url::to(['../frontend/web/images/favicon.ico'])?>" type="image/x-icon"/>
-    <!-- favicons
-    ================================================== -->
-    <link rel="apple-touch-icon" sizes="180x180" href="<?= Url::to(['../frontend/web/images/apple-touch-icon.png'])?>">
-    <link rel="icon" type="image/png" sizes="32x32" href="<?= Url::to(['../frontend/web/images/favicon-32x32.png'])?>">
-    <link rel="icon" type="image/png" sizes="16x16" href="<?= Url::to(['../frontend/web/images/favicon-16x16.png'])?>">
-<!--    <link rel="manifest" href="site.webmanifest">-->
-<!--    <script>-->
-<!--        document.documentElement.classList.remove('no-js');-->
-<!--        document.documentElement.classList.add('js');-->
-<!--    </script>-->
+    <link rel="shortcut icon" href="" type="image/x-icon"/>
+    <link rel="icon" href="" type="image/x-icon"/>
     <?php $this->registerCsrfMetaTags() ?>
     <title>CURE BEAUTY</title>
     <?php $this->head() ?>
