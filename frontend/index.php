ad6de05cca8b960c3d7218c41c82f3acd3ea4082
ahmed-12-soliman <ahmed.12.soliman@gmail.com>
2022-02-22 18:08:17 +0200

    update: return tamplet

--- frontend/index.php
+++ frontend/index.php
@@ -1,7 +1,8 @@
 <?php
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
-
+print_r("Hi from index");
+exit();
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 require(__DIR__ . '/../common/config/bootstrap.php');
@@ -12,10 +13,5 @@ $config = yii\helpers\ArrayHelper::merge(
     require(__DIR__ . '/config/main.php'),
     require(__DIR__ . '/config/main-local.php')
 );
-try {
-    $application = new yii\web\Application($config);
-} catch (\yii\base\InvalidConfigException $e) {
-    print_r($e);
-    exit();
-}
+$application = new yii\web\Application($config);
 $application->run();
